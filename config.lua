908153de363643d678be1c5b6ccc0e33f54a6b04
tonibm19 <tonibm19@gmail.com>
2014-07-25 11:14:36 +0200

    v1
    all files are uploaded now

---
+++ config.lua
@@ -0,0 +1,17 @@
+-- SETTINGS
+
+-- Show spawn enter/leave message?
+SPAWNMESSAGE = true
+
+--Disabled in spawn--
+BUILD = true
+PLACE = true
+EXPLODE = true
+MOBSPAWNING = true
+PVP = true
+FALLDAMAGE = true
+RIGHTCLICK = false
+
+-- Spawn messages --
+messageenter = "You have entered spawn!"
+messageleave = "You have left spawn, be aware of monsters!"
